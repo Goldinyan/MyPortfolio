4781c5d65fe565b722c39bbe460b14a0638a5823
Goldinyan <seifert.ansgar@gmail.com>
2025-11-20 20:27:07 +0100

    finished projects filter unit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -129,11 +129,11 @@ export default function Home() {
       demoLink: "",
     },
   ];
-  const isMdUp = useMediaQuery("(min-width: 768px)"); 
+  const isMdUp = useMediaQuery("(min-width: 768px)");
 
   useEffect(() => {
     setShowFilter(true);
-  },[isMdUp])
+  }, [isMdUp]);
 
   const router = useRouter();
 
@@ -183,14 +183,14 @@ export default function Home() {
   const resetFilter = () => {
     setTypeFilter({ demo: false, openSource: false });
     setTagsFilter([]);
-  }
+  };
 
   return (
     <>
       <div className="w-full min-h-screen  bg-bg-black bg-fixed overflow-x-hidden bg-top-splash">
-        <div className="w-full pt-10 p-4  flex flex-col items-center justify-center ">
-          <p className="text-text-third text-3xl font-bold">My Projects</p>
-          <p className="text-text-primary w-[60%] text-center font-light text-sm pt-5 px-4">
+        <div className="w-full pt-10 p-4 l:pt-20 md:pt-15  flex flex-col items-center justify-center ">
+          <p className="text-text-third text-3xl font-bold l:text-5xl md:text-4xl">My Projects</p>
+          <p className="text-text-primary l:text-md md:text-sm2 sm:w-130 text-center font-light text-sm pt-5 px-4">
             Here you can find a collection of my public/open-source projects.
             Feel free to explore and check them out!
           </p>
@@ -203,7 +203,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="bg-bg-black mt-2  md:grid md:grid-cols-2  flex-col flex w-[85%] mx-auto rounded-xl border-border-light border-1 ">
+        <div className="bg-bg-black max-w-350 mt-6 flex flex-col  w-[85%] mx-auto rounded-xl border-border-light border-1">
           <div
             onClick={() => setShowFilter((prev) => !prev)}
             className="w-[95%] bg-bg-black md:hidden p-2 hover:bg-island-bg  items-center justify-start mt-3 mb-3 flex mx-auto rounded-xl border-border-light border-1 "
@@ -216,176 +216,204 @@ export default function Home() {
             <SlidersHorizontal className="ml-auto mr-2 text-white w-5 h-5" />
           </div>
           {showFilter && (
-            <div className="mx-auto w-[95%]">
-              <div className="mt-1 ml-1 ">
-                <div className="flex items-center gap-2 flex-row">
-                  <Funnel className=" h-4 w-4 mb-2 text-text-primary" />
-                  <p className="text-text-third font-medium text-sm mb-2">
-                    Project Type
-                  </p>
-                </div>
-                <div className="flex flex-col gap-2">
-                  <label className="flex items-center gap-2 cursor-pointer">
-                    <div
-                      onClick={() =>
-                        setTypeFilter((prev) => ({ ...prev, demo: !prev.demo }))
-                      }
-                      className={`w-4 h-4 flex items-center border-text-secondary justify-center border rounded-full ${
-                        typeFilter.demo
-                          ? "bg-text-secondary text-black"
-                          : "bg-bg-black border-border-bg"
-                      }`}
-                    >
-                      {typeFilter.demo && <Check className="" />}
-                    </div>
-                    <span className="text-text-primary text-sm">Has Demo</span>
-                  </label>
-
-                  <label className="flex items-center gap-2 cursor-pointer">
-                    <div
-                      onClick={() =>
-                        setTypeFilter((prev) => ({
-                          ...prev,
-                          openSource: !prev.openSource,
-                        }))
-                      }
-                      className={`w-4 h-4 flex items-center border-text-secondary justify-center border rounded-full ${
-                        typeFilter.openSource
-                          ? "bg-text-secondary text-black"
-                          : "bg-bg-black border-border-bg"
-                      }`}
-                    >
-                      {typeFilter.openSource && <Check className="" />}
-                    </div>
-                    <span className="text-text-primary text-sm">
-                      Open Source
-                    </span>
-                  </label>
-                </div>
-              </div>
-
-              <div className="flex ml-1 mt-4 items-center gap-2 flex-row">
-                <Cpu className="text-text-primary h-4 w-4 " />
-                <p className=" text-text-third font-medium text-sm ">
-                  Technologies
-                </p>
-              </div>
-
-              <div className="mt-2 ml-1 flex flex-wrap gap-2 text-text-primary  mx-auto">
-                {Tags.map((tag) => (
-                  <div
-                    key={tag}
-                    className="hover:brightness-90"
-                    onClick={() => toggle(tag)}
-                  >
-                    <p
-                      className={`text-sm px-2 py-1  rounded-lg font-medium ${
-                        tagsFilter.includes(tag)
-                          ? " bg-text-secondary text-black"
-                          : "bg-island-bg"
-                      }`}
-                    >
-                      {tag}
+            <div className="mx-auto w-[95%] gap-5 space-y-4 grid grid-cols-1 md:grid-cols-2 l:grid-cols-4">
+             
+              {/* <div className="flex md:flex-row gap-3"> */}
+                <div className="ml-1 max-w-100   ">
+                  <div className="mt-4 flex items-center gap-2 flex-row">
+                    <Funnel className=" h-4 w-4 mb-2 text-text-primary" />
+                    <p className="text-text-third font-medium text-sm mb-2">
+                      Project Type
                     </p>
                   </div>
-                ))}
-              </div>
+                  <div className="flex flex-col gap-4 mt-2">
+                    <label className="flex items-center gap-2 cursor-pointer">
+                      <div
+                        onClick={() =>
+                          setTypeFilter((prev) => ({
+                            ...prev,
+                            demo: !prev.demo,
+                          }))
+                        }
+                        className={`w-4 h-4 flex items-center border-text-secondary justify-center border rounded-full ${
+                          typeFilter.demo
+                            ? "bg-text-secondary text-black"
+                            : "bg-bg-black border-border-bg"
+                        }`}
+                      >
+                        {typeFilter.demo && <Check className="" />}
+                      </div>
+                      <span className="text-text-primary text-sm">
+                        Has Demo
+                      </span>
+                    </label>
 
-              <div className="flex mt-4 items-center gap-2 flex-row">
-                <ArrowUpNarrowWide className="text-text-primary h-4 w-4 " />
-                <p className=" text-text-third font-medium text-sm ">Sort By</p>
-              </div>
+                    <label className="flex items-center gap-2 cursor-pointer">
+                      <div
+                        onClick={() =>
+                          setTypeFilter((prev) => ({
+                            ...prev,
+                            openSource: !prev.openSource,
+                          }))
+                        }
+                        className={`w-4 h-4 flex items-center border-text-secondary justify-center border rounded-full ${
+                          typeFilter.openSource
+                            ? "bg-text-secondary text-black"
+                            : "bg-bg-black border-border-bg"
+                        }`}
+                      >
+                        {typeFilter.openSource && <Check className="" />}
+                      </div>
+                      <span className="text-text-primary text-sm">
+                        Open Source
+                      </span>
+                    </label>
+                  </div>
+                </div>
 
-              <div className="mt-4">
-                <div className="relative bg-bg-black  p-3 items-center justify-start mt-3 mb-3 flex mx-auto rounded-xl border-border-light border-1">
-                  <div
-                    className="flex flex-row justify-between w-full cursor-pointer"
-                    onClick={() => setSelectOpen(!selectOpen)}
-                  >
-                    <p className="text-white text-xs">{sort}</p>
-                    <ArrowDown className="text-text-primary h-4 w-4 ml-1" />
+                <div className="max-w-120 pb-3 ">
+                  <div className="flex  ml-1 mt-4 items-center gap-2 flex-row">
+                    <Cpu className="text-text-primary h-4 w-4 " />
+                    <p className=" text-text-third font-medium text-sm ">
+                      Technologies
+                    </p>
                   </div>
 
-                  <div
-                    className={`absolute left-0 top-full mt-2 w-full bg-bg-black rounded-xl border border-border-light
+                  <div className="mt-2 ml-1 flex flex-wrap gap-2 text-text-primary  mx-auto">
+                    {Tags.map((tag) => (
+                      <div
+                        key={tag}
+                        className="hover:brightness-90"
+                        onClick={() => toggle(tag)}
+                      >
+                        <p
+                          className={`text-sm px-2 py-1  rounded-lg font-medium ${
+                            tagsFilter.includes(tag)
+                              ? " bg-text-secondary text-black"
+                              : "bg-island-bg"
+                          }`}
+                        >
+                          {tag}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              {/* </div> */}
+
+              {/* <div className="flex md:flex-row gap-3 "> */}
+                <div className="max-w-120 ">
+                  <div className="flex l:mt-4 items-center gap-2 flex-row">
+                    <ArrowUpNarrowWide className="text-text-primary h-4 w-4 " />
+                    <p className=" text-text-third font-medium text-sm ">
+                      Sort By
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <div className="relative bg-bg-black  p-3 items-center justify-start mt-3 mb-3 flex mx-auto rounded-xl border-border-light border-1">
+                      <div
+                        className="flex flex-row justify-between w-full cursor-pointer"
+                        onClick={() => setSelectOpen(!selectOpen)}
+                      >
+                        <p className="text-white text-xs">{sort}</p>
+                        <ArrowDown className="text-text-primary h-4 w-4 ml-1" />
+                      </div>
+
+                      <div
+                        className={`absolute left-0 top-full mt-2 w-full bg-bg-black rounded-xl border border-border-light
       transition-all duration-300 ease-out
       ${
         selectOpen
           ? "opacity-100 scale-y-100"
           : "opacity-0 scale-y-0 pointer-events-none"
       }`}
-                  >
-                    {sortValues.map((value) => (
-                      <div
-                        key={value}
-                        className="px-3 py-2 rounded-xl cursor-pointer flex items-center hover:bg-island-bg"
-                        onClick={() => {
-                          setSort(value);
-                          setSelectOpen(false);
-                        }}
                       >
-                        {sort === value ? (
-                          <Check className="mr-2 h-4 w-4 text-text-secondary" />
-                        ) : (
-                          <div className="mr-2 w-4 h-4" />
-                        )}
-                        <p className="text-white text-sm">{value}</p>
+                        {sortValues.map((value) => (
+                          <div
+                            key={value}
+                            className="px-3 py-2 rounded-xl cursor-pointer flex items-center hover:bg-island-bg"
+                            onClick={() => {
+                              setSort(value);
+                              setSelectOpen(false);
+                            }}
+                          >
+                            {sort === value ? (
+                              <Check className="mr-2 h-4 w-4 text-text-secondary" />
+                            ) : (
+                              <div className="mr-2 w-4 h-4" />
+                            )}
+                            <p className="text-white text-sm">{value}</p>
+                          </div>
+                        ))}
                       </div>
-                    ))}
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="flex ml-1 mt-4 items-center gap-2 flex-row">
-                <ChartNoAxesColumn className="text-text-primary h-4 w-4 " />
-                <p className=" text-text-third font-medium text-sm ">Results</p>
-              </div>
+                <div className="max-w-120 ">
+                  <div className="flex ml-1 l:mt-4  items-center gap-2 flex-row">
+                    <ChartNoAxesColumn className="text-text-primary h-4 w-4 " />
+                    <p className=" text-text-third font-medium text-sm ">
+                      Results
+                    </p>
+                  </div>
 
-              <div className="flex flex-col gap-2">
-                <div className="mt-2">
-                  <p className="text-text-primary text-sm">
-                    Showing {filProjects.length} of {projects.length} projects
-                  </p>
-                </div>
-                <div>
-                  <div className="mt-1 ml-1 flex flex-wrap gap-2 text-text-primary  mx-auto">
-                    {tagsFilter.map((tag) => (
-                      <div
-                        key={tag}
-                        className="hover:brightness-90 items-center rounded-lg px-2 py-1 flex flex-row bg-dark-bg border-1 border-border-bg"
-                        onClick={() =>
-                          setTagsFilter((prev) => prev.filter((t) => t !== tag))
-                        }
-                      >
-                        <p className={`text-sm px-2 rounded-lg font-medium `}>
-                          {tag}
-                        </p>
-                        <X className="text-text-primary h-4 w-4 " />
+                  <div className="flex flex-col gap-2">
+                    <div className="mt-2">
+                      <p className="text-text-primary text-sm">
+                        Showing {filProjects.length} of {projects.length}{" "}
+                        projects
+                      </p>
+                    </div>
+                    <div>
+                      <div className="mt-1 ml-1 flex flex-wrap gap-2 text-text-primary  mx-auto">
+                        {tagsFilter.map((tag) => (
+                          <div
+                            key={tag}
+                            className="hover:brightness-90 items-center rounded-lg px-2 py-1 flex flex-row bg-dark-bg border-1 border-border-bg"
+                            onClick={() =>
+                              setTagsFilter((prev) =>
+                                prev.filter((t) => t !== tag)
+                              )
+                            }
+                          >
+                            <p
+                              className={`text-sm px-2 rounded-lg font-medium `}
+                            >
+                              {tag}
+                            </p>
+                            <X className="text-text-primary h-4 w-4 " />
+                          </div>
+                        ))}
                       </div>
-                    ))}
+                    </div>
+                    <div
+                      onClick={() => resetFilter()}
+                      className={`${
+                        checkFilters()
+                          ? " border-border-light cursor-pointer"
+                          : "border-border-bg  cursor-not-allowed"
+                      } w-full  gap-3  justify-center  p-2  items-center  mb-3 flex mx-auto rounded-xl  border-1 `}
+                    >
+                      <RotateCcw
+                        className={`${
+                          checkFilters() ? "text-white" : "text-text-primary"
+                        } h-4 w-4 `}
+                      />
+
+                      <p
+                        className={`${
+                          checkFilters() ? "text-white" : "text-text-primary"
+                        } `}
+                      >
+                        Reset All Filters
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <div
-                onClick={() => resetFilter()}
-                  className={`${
-                    checkFilters()
-                      ? " border-border-light cursor-pointer"
-                      : "border-border-bg  cursor-not-allowed"
-                  } w-full  gap-3  justify-center  p-2  items-center  mb-3 flex mx-auto rounded-xl  border-1 `}
-                >
-                  <RotateCcw className={`${
-                    checkFilters()
-                      ? "text-white"
-                      : "text-text-primary"
-                  } h-4 w-4 `} />
-
-                  <p className={`${
-                    checkFilters()
-                      ? "text-white"
-                      : "text-text-primary"
-                  } `} >Reset All Filters</p>
-                </div>
-              </div>
+              
+              {/* </div> */}
             </div>
           )}
         </div>
@@ -409,4 +437,3 @@ export default function Home() {
     </>
   );
 }
-
